aa28d2d465fe493c61c008682a1f4d0084638c56
Amshu0-0 <amshuw05@gmail.com>
2025-05-18 02:10:38 -0500

    Made Project files

---
+++ website/src/pages/OurProjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurProjects = () => {
+    return (
+        <div>
+            <h2>This is the Our Projects page.</h2>
+        </div>
+    );
+}
+
+export default OurProjects;
